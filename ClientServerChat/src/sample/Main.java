bdbe947a031a37d30f0c6c7b8fa693620fde127b
luisale06 <86024351+luisale06@users.noreply.github.com>
2021-08-29 13:09:45 -0600

    Update Main.java

--- ClientServerChat/src/sample/Main.java
+++ ClientServerChat/src/sample/Main.java
@@ -1,1 +1,22 @@
+package sample;
 
+/**
+ *imports implemented in the application
+ * @author Luis Alejandro Barreda
+ */
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.ObjectOutputStream;
+import java.net.*;
+import java.io.*;
+
+/**
+ * Client class
+ */
+public class Main extends Application implements Runnable{
