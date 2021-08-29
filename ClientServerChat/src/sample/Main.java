498466913a993ef420c38dce02bc81ecefc1b2c7
luisale06 <86024351+luisale06@users.noreply.github.com>
2021-08-29 13:15:11 -0600

    Update Main.java

--- ClientServerChat/src/sample/Main.java
+++ ClientServerChat/src/sample/Main.java
@@ -39,3 +39,58 @@ public class Main extends Application implements Runnable{
      */
     @Override
     public void start(Stage chat){
+
+        /**
+         * Text Area from Java FX generation and location specifications
+         */
+        clientarea = new TextArea();
+        clientarea.setTranslateX(0);
+        clientarea.setTranslateY(0);
+
+        /**
+         * Labels from Java FX generation and location specifications
+         */
+        lblval = new Label("Enter the value");
+        lblval.setTranslateX(0);
+        lblval.setTranslateY(-50);
+
+        lblwght = new Label("Enter the weight");
+        lblwght.setTranslateX(0);
+        lblwght.setTranslateY(-25);
+
+        lbltax = new Label("Enter the tax");
+        lbltax.setTranslateX(0);
+        lbltax.setTranslateY(5);
+
+        /**
+         * Entries from Java FX generation, location, and dimension specifications
+         */
+        value_entry = new TextField();
+        value_entry.setTranslateX(100);
+        value_entry.setTranslateY(20);
+        value_entry.setMaxHeight(20);
+        value_entry.setMaxWidth(70);
+
+        weight_entry = new TextField();
+        weight_entry.setTranslateX(100);
+        weight_entry.setTranslateY(40);
+        weight_entry.setMaxHeight(20);
+        weight_entry.setMaxWidth(70);
+
+        tax_entry = new TextField();
+        tax_entry.setTranslateX(100);
+        tax_entry.setTranslateY(60);
+        tax_entry.setMaxHeight(20);
+        tax_entry.setMaxWidth(70);
+
+        /**
+         * Button from Java FX generation and location specifications
+         */
+        btn = new Button("Send: ");
+        btn.setTranslateX(220);
+        btn.setTranslateY(-50);
+
+        /**
+         * Button´s action. Opens a socket to send,  as a class, the details written in the entries
+         */
+        btn.setOnAction(event -> {
