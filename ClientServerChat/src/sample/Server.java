4589b2e96ee2fd889fefd0c0420102d896c9c301
luisale06 <86024351+luisale06@users.noreply.github.com>
2021-08-29 13:34:16 -0600

    Update Server.java

--- ClientServerChat/src/sample/Server.java
+++ ClientServerChat/src/sample/Server.java
@@ -1,1 +1,20 @@
+package sample;
 
+/**
+ *imports implemented in the application
+ * @author Luis Alejandro Barreda
+ */
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Server class
+ */
+public class Server extends Application implements Runnable {
